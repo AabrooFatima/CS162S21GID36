06a795af0c84b63006422b0d62ddd42b90d6b62e
AabrooFatima <81793791+AabrooFatima@users.noreply.github.com>
2021-06-26 15:20:05 +0500

    remaining guis

--- HomeServiceProvision/Models/Receipt.cs
+++ HomeServiceProvision/Models/Receipt.cs
@@ -13,6 +13,6 @@ namespace HomeServiceProvision.Models
         public static List<Services> ElectricianServices = new List<Services>();
         public static List<Services> CookServices = new List<Services>();
         public static List<Services> CleanerServices = new List<Services>();
-        public static List<Services> PlumberServices = new List<Services>();
+        public static List<Services> PlumbersServices = new List<Services>();
     }
 }
